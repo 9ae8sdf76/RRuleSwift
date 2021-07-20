ec874bef975485a0b05873964b5ef60166a67df2
HuongCao-agilityio <31909427+HuongCao-agilityio@users.noreply.github.com>
2021-07-20 11:36:00 +0700

    Create Package.swift
    Add package file

---
+++ Package.swift
@@ -0,0 +1,19 @@
+import PackageDescription
+
+let package = Package(
+    name: "RRuleSwift",
+    platforms: [.iOS(.v9)],
+    products: [
+        .library(
+            name: "RRuleSwift",
+            targets: ["RRuleSwift"]
+        ),
+    ],
+    targets: [
+        .target(
+            name: "RRuleSwift",
+            dependencies: [],
+            path: "RRuleSwift/"
+        )
+    ]
+)
